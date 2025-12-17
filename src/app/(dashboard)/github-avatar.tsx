20b68639e5d1eebcf5fe310a0c2c453e9e609267
bmthd <jougennotuki67@gmail.com>
2025-12-17 11:59:39 +0000

    fix: AvatarをLinkに

--- src/app/(dashboard)/github-avatar.tsx
+++ src/app/(dashboard)/github-avatar.tsx
@@ -1,9 +1,16 @@
+"use client"; // TODO: SCにしたい
+
 import { Avatar, type AvatarProps } from "@yamada-ui/react";
+import { NextLink } from "@/ui";
 
 interface MyAvatarProps extends AvatarProps {
 	username: string;
 }
 
 export const GithubAvatar = ({ username, ...props }: MyAvatarProps) => {
-	return <Avatar src={`https://github.com/${username}.png`} name={username} {...props} />;
+	return (
+		<NextLink href={`https://github.com/${username}`} external>
+			<Avatar src={`https://github.com/${username}.png`} name={username} {...props} />
+		</NextLink>
+	);
 };
